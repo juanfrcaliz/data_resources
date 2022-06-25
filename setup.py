fb381c8a507fbd4f6234f8483c09f2aa0e93423e
Juanfrcaliz <35780297+juanfrcaliz@users.noreply.github.com>
2022-06-25 13:27:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name='data_resources',
       version='1.0',
       description='Resources for working with data',
       author='juanfrcaliz',
-      packages=['corr_limit', 'data_visualize', 'db_connect', 
-                'impute_missing_values', 'model_score', 'nlp_tools'],
+      packages=['data_resources'],
      )
