465c2ee9cc3512f92ae9e71427f2ff62149fa0bf
Juanfrcaliz <35780297+juanfrcaliz@users.noreply.github.com>
2022-06-25 13:25:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='data_resources',
+      version='1.0',
+      description='Resources for working with data',
+      author='juanfrcaliz',
+      packages=['corr_limit', 'data_visualize', 'db_connect', 
+                'impute_missing_values', 'model_score', 'nlp_tools],
+     )
